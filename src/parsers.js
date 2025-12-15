5864038680dec5ee288e7344022d2d1c908e8652
Anyakin666 <aninasvi@gamil.com>
2025-12-15 16:31:17 +0300

    step 4 + raznye extensions

--- src/parsers.js
+++ src/parsers.js
@@ -4,5 +4,11 @@ import path from 'path';
 export function readFile(filepath) {
   const absolutePath = path.resolve(process.cwd(), filepath);
   const content = fs.readFileSync(absolutePath, 'utf-8');
+  const extension = path.extname(filepath).toLowerCase();
+  
+  if (extension !== '.json') {
+    throw new Error(`Unsupported file format: ${extension}. Only .json is supported`);
+  }
+  
   return JSON.parse(content);
 }
