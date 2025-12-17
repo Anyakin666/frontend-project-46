98cbbeed98e4bd23b8c8e30c63bab291c60ea85a
Anyakin666 <aninasvi@gamil.com>
2025-12-17 16:27:01 +0300

    returned babel

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
-export default {
+module.exports = {
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
   testMatch: ['**/__tests__/**/*.test.js'],
+  collectCoverageFrom: ['src/**/*.js'],
+  coverageDirectory: 'coverage',
 };
